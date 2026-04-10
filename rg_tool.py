435971ab969a96b38836add1d83b661855805c69
DynaMight1124 <dynamight@gmail.com>
2026-04-10 18:58:05 +0100

    Reduced flash size
    Didnt need as much space, also small amendment to mute the background slightly

--- rg_tool.py
+++ rg_tool.py
@@ -27,7 +27,7 @@ PROJECT_APPS = {
   'gwenesis':     [0, 16, 1048576],
   'fmsx':         [0, 16, 655360],
   'gbsp':         [0, 16, 851968],
-  'celeste':      [0, 16, 655360],
+  'celeste':      [0, 16, 524288],
 }
 # PROJECT_APPS = {}
 # for t in glob.glob("*/CMakeLists.txt"):
